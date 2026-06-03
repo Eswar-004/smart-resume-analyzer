5feb776f38a3362f24b83295248a8d2a15c8cde2
Eswar-004 <eshwar2889@gmail.com>
2026-06-03 13:09:58 +0530

    updated db.py

--- db.py
+++ db.py
@@ -6,11 +6,11 @@ load_dotenv()
 
 def get_connection():
     return pymysql.connect(
-        host=os.getenv("MYSQLHOST"),
-        port=int(os.getenv("MYSQLPORT", 3306)),
-        user=os.getenv("MYSQLUSER"),
-        password=os.getenv("MYSQLPASSWORD"),
-        database=os.getenv("MYSQLDATABASE"),
+        host=os.getenv("MYSQLHOST") or os.getenv("DB_HOST"),
+        port=int(os.getenv("MYSQLPORT") or os.getenv("DB_PORT", 3306)),
+        user=os.getenv("MYSQLUSER") or os.getenv("DB_USER"),
+        password=os.getenv("MYSQLPASSWORD") or os.getenv("DB_PASSWORD"),
+        database=os.getenv("MYSQLDATABASE") or os.getenv("DB_NAME"),
         cursorclass=pymysql.cursors.DictCursor,
         autocommit=False
     )
